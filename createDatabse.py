78b6939144f0c2a4fa92ae3a412220dc6b6eb035
omer-mohamed <omer.javed26@.com>
2023-02-22 20:37:21 -0500

    Added a try/catch and updated the animal list

--- createDatabse.py
+++ createDatabse.py
@@ -1,20 +1,27 @@
 import json
 import wikipedia
 
+
 data = []
 
 with open("test.txt") as f:
     for title in f:
         title = title.strip()
-        page = wikipedia.page(title + " Animal")
-        summary = page.summary
-        content = page.content.split("== References ==")[0].split("== See also ==")[0].split("== Footnotes ==")[0]
+        try:
+            page = wikipedia.page(title + " Animal")
+            summary = page.summary
+            content = page.content.split("== References ==")[0].split("== See also ==")[0].split("== Footnotes ==")[0]
+            
+            data = {
+                "title": title,
+                "summary": summary,
+                "content": content
+            }
 
-        data.append({
-            "title": title,
-            "summary": summary,
-            "content": content
-        })
+            # Do something with the data
+            
+        except wikipedia.exceptions.PageError as e:
+            print(f"Page not found: {title}")
 
 with open("output.json", "w") as f:
     json.dump(data, f)
